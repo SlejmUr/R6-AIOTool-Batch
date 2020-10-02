8cff30d0d6397a66a895ee44677fadbbf801d3af
SlejmUr <52343819+SlejmUr@users.noreply.github.com>
2020-10-02 12:56:56 +0200

    Update Install.bat

--- Installer/Install.bat
+++ Installer/Install.bat
@@ -1,14 +1,12 @@
 @echo off
-chcp 65001
 setlocal enableextensions enabledelayedexpansion
 set homepath=%cd%
-
 
 MODE 78,20
 echo ------------------------------------------------------------------------------
 echo                              Downloading AllInOne Tools...
 echo ------------------------------------------------------------------------------
-curl -L  "https://github.com/SlejmUr/R6-AIOTool/raw/master/Release/V1.1/R6AllInOne.bat" --output AllInOneTools.bat
+curl -L  "https://github.com/SlejmUr/R6-AIOTool/raw/master/Release/V1.2/AllInOneTools.bat" --output AllInOneTools.bat
 move AllInOneTools.bat ..
 cls
 exit
